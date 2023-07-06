bb6113631c7f03661c7ccda26c460e8f22c85c94
aehrlich1 <anatol.ehrlich@gmail.com>
2023-07-06 16:26:10 +0200

    added script to execute program on slurm

--- submission/submit.sh
+++ submission/submit.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Accepts 1 parameter: finetune.py OR molclr.py
-
 # 1. ssh into titan directory: /home/mescalin/aehrlich/src/iMolCLR
 # 2. git pull
 # 3. execute submit_job.slrm: sbatch submit_job.slrm
+
+ssh aehrlich@titan.tbi.univie.ac.at 'cd src/iMolCLR; git pull; ./submission/submit_imolclr.slrm'
